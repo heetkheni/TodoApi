9971845c3af7c3ba8e41e55296e0dc5e47754895
Heet Kheni <heetkheni47@gmail.com>
2023-12-30 23:12:36 +0530

    done

--- data/database.js
+++ data/database.js
@@ -5,3 +5,7 @@ export const databaseConnection  =() => {
     dbName: "BackendApi",
 }).then(() => console.log("Database Connectetd")).catch((e) => console.log(e));
 } 
+
+
+// heetkheni47
+// Ze1wa1fHifmTp6oc
